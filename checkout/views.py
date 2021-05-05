fa1b18f43e1bf29d1d151cc0d7574ea9989dcf5f
Rozdandy <rozdandy@gmail.com>
2021-05-05 17:58:40 +0000

    Added stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51InnIvFl4lG8CSXrigliIUwiucMHa84VL56Mb6hdz0i1kfzB1caCiAB1Zf6gz2HeeI92cE6i8wKc3034MKlfGTTC00V0iYs0mq', 
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, template, context)
